c5d581249a8b42e807c436cd752eff2d5b6e1206
th30retical <tfjtan@hotmail.com>
2016-04-04 21:45:16 -0400

    removed comments

--- Server.java
+++ Server.java
@@ -41,36 +41,6 @@ public class Server {
             System.exit(1);
         }
         
-
-          
-             
-        
-/*
-        Socket clientSocket = null;
-        try {
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
-
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-				new InputStreamReader(
-				clientSocket.getInputStream()));
-        String inputLine, outputLine;
-        
-        
-        while ((inputLine = in.readLine()) != null) {
-        	 out.println("50,50,200,200");
-        	 points.add(inputLine);
-             System.out.println(inputLine);
-             
-        }
-        out.close();
-        in.close();
-        clientSocket.close();
-        serverSocket.close();*/
     }
     public static Color randomColours(){
     	Random rand=new Random();
